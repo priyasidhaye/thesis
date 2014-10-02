19e6b36952b2a1c3c73b8083cddfa36f03f95d63
priyasidhaye <priya.sidhaye@gmail.com>
2014-10-02 00:21:04 -0400

    Converted config file to json string
    script reads from json string for OAuth tokens.

--- twitter_trials/try.py
+++ twitter_trials/try.py
@@ -1,21 +1,25 @@
 import simplejson
 import twitter
 import oauth2 as oauth
+import simplejson as json
 
 #Reading from file
 with open('.config','r') as config_file :
+		json_obj = json.loads(config_file.readline())
+		c_key = json_obj["consumer_key"] 
+		c_secret = json_obj["consumer_secret"]
+		a_token = json_obj["access_token_key"]
+		a_token_secret = json_obj["access_token_secret"]
+	
 		api = twitter.Api(
-			consumer_key = config_file.readline().strip(),
-			consumer_secret = config_file.readline().strip(),
-			access_token_key = config_file.readline().strip(),
-			access_token_secret = config_file.readline().strip()
+			consumer_key = c_key,
+			consumer_secret = c_secret,
+			access_token_key = a_token,
+			access_token_secret = a_token_secret
 			) 
 
-search = api.GetSearch(term='#freecoffee', lang='en', result_type='recent', count=100, max_id='') 
+search = api.GetSearch(term='#harrypotter', lang='en', result_type='recent', count=100, max_id='') 
 for t in search:
 	print t.user.screen_name
 	print t.text.encode('utf-8')
-	print 
-
-
 
