7611051902b136f6234bf217046e2c8bc2fd4985
priyasidhaye <priya.sidhaye@gmail.com>
2014-10-02 00:26:40 -0400

    Getting rid of useless variables.
    Script reads from config file JSON string

--- twitter_trials/try.py
+++ twitter_trials/try.py
@@ -6,18 +6,14 @@ import simplejson as json
 #Reading from file
 with open('.config','r') as config_file :
 		json_obj = json.loads(config_file.readline())
-		c_key = json_obj["consumer_key"] 
-		c_secret = json_obj["consumer_secret"]
-		a_token = json_obj["access_token_key"]
-		a_token_secret = json_obj["access_token_secret"]
-	
 		api = twitter.Api(
-			consumer_key = c_key,
-			consumer_secret = c_secret,
-			access_token_key = a_token,
-			access_token_secret = a_token_secret
-			) 
+			consumer_key = json_obj["consumer_key"],
+			consumer_secret = json_obj["consumer_secret"],
+			access_token_key = json_obj["access_token_key"],
+			access_token_secret = json_obj["access_token_secret"]
+		) 
 
+#Actual request and print to terminal
 search = api.GetSearch(term='#harrypotter', lang='en', result_type='recent', count=100, max_id='') 
 for t in search:
 	print t.user.screen_name
