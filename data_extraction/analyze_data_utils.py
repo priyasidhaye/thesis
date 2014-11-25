c45192da9875d76f28f931b087ee8e2768592458
priyasidhaye <priya.sidhaye@gmail.com>
2014-11-25 16:45:16 -0500

    Refactored all code and wrote functions to handle data

---
+++ data_extraction/analyze_data_utils.py
@@ -0,0 +1,110 @@
+import simplejson as json
+import matplotlib.pyplot as plt
+import newspaper
+from newspaper import Article
+
+url_id_count = 0
+
+#Returns the numbero of unique users
+def number_of_unique_users(filename) :
+	with open(filename, 'r') as data_obj :
+		data = json.load(data_obj) 
+		unique_users = []
+		for search_term in data : 
+			tweets = data[search_term]
+			for tweet_id in tweets :
+				tweet = tweets[tweet_id]
+				if tweet['user']['id'] not in unique_users :
+					unique_users.append(tweet['user']['id'])
+	return len(unique_users)
+				
+#Currently returns number of tweets and tweets with urls in it.
+def get_numbers_on_file(filename) :
+	with open(filename, 'r') as data_obj : 
+		data = json.load(data_obj)
+		count_of_tweets = 0
+		count_of_tweets_with_links = 0
+		for search_term in data : 
+			tweets = data[search_term]
+			count_of_tweets += len(tweets.keys())
+			for tweet_id in tweets.keys() :
+				tweet = tweets[tweet_id]
+				if 'urls' in tweet.keys() :
+					count_of_tweets_with_links += 1
+	return [count_of_tweets, count_of_tweets_with_links]
+
+#Currently, returns the nummber of times a hashtag has been used and the number of unique hashtags
+def hashtag_numbers(filename) :
+	with open(filename, 'r') as data_obj :
+		data = json.load(data_obj)
+		hashtags = {}
+		for search_term in data :
+			tweets = data[search_term]
+			for tweet_id in tweets :
+				tweet = tweets[tweet_id]
+				if 'hashtags' in tweet.keys() :
+					for hashtag in tweet['hashtags'] :
+						if hashtag in hashtags.keys() :
+							hashtags[hashtag] += 1
+						else :						
+							hashtags[hashtag] = 1
+		plt.bar(range(len(hashtags)), hashtags.values(), align='center')
+		plt.xticks(range(len(hashtags)), hashtags.keys())
+		plt.show()
+
+	return len(hashtags.keys())
+
+#writes all unique urls to a file?
+def url_experiments (filename, op_file) :
+	url_dict = {}
+	with open(filename, 'r') as data_obj :
+		data = json.load(data_obj)
+		for search_term in data :
+			tweets = data[search_term]
+			for tweet_id in tweets :
+				if 'urls' in tweets[tweet_id] :
+					for t_url in tweets[tweet_id]['urls'] :
+						  url_dict[tweet_id] = tweets[tweet_id]['urls'][t_url]
+	with open(op_file, 'w') as op_fileobj :
+		op_fileobj.write(json.dumps(url_dict))
+			
+	return
+
+def url_flip (filename, op_file) :
+	url_flipped = {}
+	with open(filename, 'r') as data_obj :
+		data = json.load(data_obj)
+		for tweet_id, url in data.items() :
+			if url not in url_flipped.keys() : 
+				url_flipped[url] = [tweet_id]
+			else :
+				url_flipped[url].append(tweet_id)
+	print len(url_flipped.keys())
+	with open(op_file, 'w') as flip_url :
+		flip_url.write(json.dumps(url_flipped))
+	return
+
+#Fetches the actual text of the urls. 
+#First trial run to see how many come up with decent texts
+def fetch_url_data(filename, op_file) :
+	urls = {}
+	new_urls = {}
+	with open(filename, 'r') as url_file : 
+		urls = json.load(url_file)
+	for url in urls :
+		new_id = generate_id(url)
+		temp = {}
+		temp['url'] = url
+		article = Article(url, 'en')
+		article.download()
+		article.parse()
+		temp['title'] = article.title
+		temp['text'] = article.text
+		temp['tweets'] = urls[url]
+		print article.title
+		new_urls[new_id] = temp
+	with open(op_file,'w') as output_file :
+		output_file.write(json.dumps(new_urls))
+
+def generate_id(url):
+	return url_id_count
--- data_extraction/data_utils.py
+++ data_extraction/data_utils.py
@@ -17,3 +17,54 @@ def write_tweets_to_file (tweetsets, filename) :
 		op_file.write(json.dumps(tweetsets))
 	return
 
+#Writes a display-ready json file in folder
+def write_tweets_for_display(input_file, filename) : 
+	tweetsets = {}
+	with open(input_file, 'r') as data_file :	
+		tweetsets = json.load(data_file)
+	tweetset_disp= {}
+	for search_term in tweetsets.keys() : 
+		tweets = tweetsets[search_term] 
+		tweet_dict = {}
+		for tweet_id in tweets.keys() : 
+			tweet = tweets[tweet_id]
+			tweet_disp = {}
+			tweet_disp['text'] = tweet['text']
+			tweet_disp['created_at'] = tweet['created_at']
+			if 'hashtags' in tweet.keys() : 
+				tweet_disp['hashtags'] = tweet['hashtags']
+			if 'retweeted' in tweet.keys() : 
+				tweet_disp['retweeted'] = tweet['retweeted']
+			if 'urls' in tweet.keys() : 
+				tweet_disp['urls'] = tweet['urls']
+			if 'retweet_count' in tweet.keys() :
+				tweet_disp['retweet_count'] = tweet['retweet_count']
+			tweet_dict[tweet_id] = tweet_disp
+		tweetset_disp[search_term] = tweet_dict
+	with open(filename, 'w') as op_file :
+		op_file.write(json.dumps(tweetset_disp))
+	return
+
+#Add only unique tweets to a total data file
+def append_unique_tweets_data(input_file, data_file) : 
+	#build hash of ids in one search term. 
+	with open(data_file, 'r') as data : 
+		all_data = json.load(data)
+		with open(input_file, 'r') as input_fileobj : 
+			current_data = json.load(input_fileobj)
+			for search_term in current_data.keys() : 
+				if search_term in all_data.keys() : 
+					#check for non-repeating ids and append
+					all_data[search_term] = append_unique_tweets(all_data[search_term], current_data[search_term])
+				else :
+					all_data[search_term] = current_data[search_term]
+	with open(data_file, 'w') as op : 
+		op.write(json.dumps(all_data))
+	return
+
+#Helper method
+def append_unique_tweets(compare_data, current_data) : 
+	for tweet_id in current_data.keys() : 
+		if not(tweet_id in compare_data.keys()) : 
+			compare_data[tweet_id] = current_data[tweet_id]
+	return compare_data
